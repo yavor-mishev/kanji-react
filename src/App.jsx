c1ee17e4de6521e6549a8f252680b3ba71de61ab
Yavor Mishev <123746838+yavor-mishev@users.noreply.github.com>
2025-03-17 15:54:13 +0200

    fix courses page visability

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
-import Courses from "./pages/courses/courses";
+import Courses from "./pages/courses/courses.jsx";
 import Chat from "./pages/chat";
 import Settings from "./pages/settings";
 import Layout from "./Layout";
